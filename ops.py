e72a6fcd3ce2569495342620a02fe61a6f3ef502
bonjorno7 <jorijndegraaf@gmail.com>
2022-09-12 01:05:07 +0200

    Update operator to work with the new utils
    I'm also changing how I import things because I think it reads better.
    And I moved the tag_redraw functionality here, because it felt weird putting it in utils now that it's a comprehensive class.

--- ops.py
+++ ops.py
@@ -1,26 +1,27 @@
-import bpy
-from . import utils
+from bpy.types import Context, Object, Operator
+from bpy.props import BoolProperty
+from .utils import Uninstancer
 
 
-class OBJECT_OT_restore_collection_instances(bpy.types.Operator):
+class OBJECT_OT_restore_collection_instances(Operator):
     bl_idname = 'object.restore_collection_instances'
     bl_label = 'Restore Collection Instances'
     bl_description = 'Replace instances with collections, using duplicate objects'
     bl_options = {'REGISTER', 'UNDO'}
 
-    duplicate: bpy.props.BoolProperty(
+    duplicate: BoolProperty(
         name='Duplicate',
         description='Duplicate instance collection, its children, and the objects inside',
         default=True,
     )
 
-    linked: bpy.props.BoolProperty(
+    linked: BoolProperty(
         name='Linked',
         description='Duplicate object but not object data, linking to the original data',
         default=True,
     )
 
-    def draw(self, context: bpy.types.Context):
+    def draw(self, context: Context):
         layout = self.layout
         layout.use_property_split = True
         layout.use_property_decorate = False
@@ -30,18 +31,19 @@ class OBJECT_OT_restore_collection_instances(bpy.types.Operator):
         sub.enabled = self.duplicate
         sub.prop(self, 'linked')
 
-    def execute(self, context: bpy.types.Context) -> set:
-        objects: list[bpy.types.Object] = context.selected_objects[:]
-        bpy.ops.object.select_all(action='DESELECT')
+    def execute(self, context: Context) -> set:
+        uninstancer = Uninstancer(self.duplicate, self.linked)
 
-        for object in objects:
-            if object.type != 'EMPTY':
-                continue
-            if object.instance_type != 'COLLECTION':
-                continue
-            if not object.instance_collection:
-                continue
-            utils.realize_instance(context, object, self.duplicate, self.linked)
+        for object in context.selected_objects.copy():
+            object: Object
 
-        utils.tag_redraw(context)
+            if object.type == 'EMPTY':
+                if object.instance_type == 'COLLECTION':
+                    if object.instance_collection:
+                        uninstancer.uninstance(object)
+
+        for window in context.window_manager.windows.values():
+            for area in window.screen.areas.values():
+                area.tag_redraw()
+
         return {'FINISHED'}
